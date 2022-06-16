f50cf1727ee9f1b003c55bbd6afca8b167528ba7
Kevin Wooten <kevin@wooten.com>
2022-06-16 00:17:55 -0700

    Add failing tests that reproduce message conversion issues
    This covers issues with message conversion failures related to both MANUAL and POST_PROCESSING ack strategies.

---
+++ smallrye-reactive-messaging-rabbitmq/src/test/java/io/smallrye/reactive/messaging/rabbitmq/ConvertedConsumptionWithManualAckBean.java
@@ -0,0 +1,32 @@
+package io.smallrye.reactive.messaging.rabbitmq;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.reactive.messaging.Acknowledgment;
+import org.eclipse.microprofile.reactive.messaging.Acknowledgment.Strategy;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+
+/**
+ * A bean that can be registered to support consumption of messages from an
+ * incoming rabbitmq channel that uses a message converter.
+ *
+ * Uses the MANUAL ack strategy.
+ */
+@ApplicationScoped
+public class ConvertedConsumptionWithManualAckBean {
+
+    private final List<Integer> list = new ArrayList<>();
+
+    @Incoming("data")
+    @Acknowledgment(Strategy.MANUAL)
+    public void process(Integer input) {
+        list.add(input);
+    }
+
+    public List<Integer> getResults() {
+        return list;
+    }
+}
